9bb3fa0c8ee1aa6972ecdd963f7f4659c5656413
ryanzheng2 <168475442+ryanzheng2@users.noreply.github.com>
2024-08-28 22:59:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // constant variables
 // const PORT = process.env.PORT_NUM;
-const PORT = process.argv [2];
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 const transporter = nodemailer.createTransport({
     service: 'gmail',
